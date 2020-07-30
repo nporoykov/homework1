10e5754821d198c0cb2c88621da307185e2f5bc2
Nikita P <nporoykov@gmail.com>
2020-07-30 23:57:21 +0300

    commit final

--- src/test/java/HomeworkTest.java
+++ src/test/java/HomeworkTest.java
@@ -29,8 +29,8 @@ public class HomeworkTest {
 
     @Before
     public void setUp() {
-      //  WebDriverManager.chromedriver().setup();
-      //    driver = new ChromeDriver();
+        //WebDriverManager.chromedriver().setup();
+        //driver = new ChromeDriver();
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         logger.info("Драйвер поднят");
@@ -43,225 +43,62 @@ public class HomeworkTest {
       //  Да, спасибо
         try{
             new WebDriverWait(driver,60).until(ExpectedConditions.visibilityOfElementLocated(new By.ByXPath("//span[text()='Да, спасибо']/parent::button"))).click();
-        }catch (Exception e){logger.info("нев выбора региона");}
+            logger.info("Подтвержден регион");
+        }catch (Exception e){logger.info("Нет выбора региона");}
        // new WebDriverWait(driver,60).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//span[text()='Да, спасибо']/parent::button"))).click();
       //  new WebDriverWait(driver,60).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//button[@id='27903767-tab']"))).click();
      //   new WebDriverWait(driver,4).until(ExpectedConditions.visibilityOfElementLocated(new By.ByXPath("//a[text()='Мобильные телефоны']"))).click();
         new WebDriverWait(driver,60).until(ExpectedConditions.visibilityOfElementLocated(new By.ByXPath("//span[text()='HONOR']"))).click();
+        logger.info("Фильтрация по HONOR");
         new WebDriverWait(driver,4).until(ExpectedConditions.visibilityOfElementLocated(new By.ByXPath("//span[text()='Xiaomi']"))).click();
+        logger.info("+ Фильтрация по Xiaomi");
         new WebDriverWait(driver,4).until(ExpectedConditions.visibilityOfElementLocated(new By.ByXPath("//button[text()='по цене']"))).click();
+        logger.info("Фильтрация по цене(от меньшей к большей)");
         new WebDriverWait(driver, 10).until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div._2LvbieS_AO._1oZmP3Lbj2")));
-        // String locator = "article[6]/div[2]/div/div";
-        //WebElement icon = driver.findElement(By.xpath(locator));
-        //Actions actions = new Actions(driver);
-        //actions.moveToElement(icon).click().build().perform();
-        // String locator = "//img[contains(@alt,'Смартфон HONOR')]/parent::a";
-       // Actions builder = new Actions(driver);
-      //  builder.moveToElement(driver.findElement(By.xpath(locator))).perform();
-      //  driver.findElement(By.xpath("//img[contains(@alt,'Смартфон HONOR')]/parent::a/preceding-sibling::div/div[1]")).click();
-      //  driver.findElement(By.xpath("//article[8]/div[2]/div[1]/div")).click();
-       //  new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//img[contains(@title,'Смартфон HONOR')]/parent::a/preceding-sibling::div/div[1]/div"))).click();
-    //  driver.findElement(By.xpath("//img[contains(@title,'Смартфон HONOR')]/parent::a/preceding-sibling::div/div[1]/div")).click();
-      driver.findElement(By.xpath("//a[contains(@title,'Смартфон HONOR')]/ancestor::article//div[contains(@aria-label,'сравнению')]")).click();
-      String checkString1 = new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Товар Смартфон HONOR')]"))).getText();
-        Assert.assertThat(checkString1, containsString("добавлен к сравнению"));
+        logger.info("Подождали пока скроется loader");
 
-      //  driver.findElement(By.xpath("//article[5]/div[2]/div[1]/div")).click();
-        driver.findElement(By.xpath("//a[contains(@title,'Смартфон Xiaomi Redmi')]/ancestor::article//div[contains(@aria-label,'сравнению')]")).click();
-     String str2 = driver.findElement(By.xpath("//a[contains(@title,'Смартфон Xiaomi Redmi')]")).getAttribute("title").toString();
-        String fin2 = "Товар " + str2 + " добавлен к сравнению";
-        //   new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(new By.ByXPath("//img[contains(@title,'Смартфон Xiaomi Redmi')]/parent::a/preceding-sibling::div/div[1]/div"))).click();
-          String checkString2 = new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Товар Смартфон Xiaomi Redmi')]"))).getText();
-        // Assert.assertThat(checkString2, containsString("добавлен к сравнению"));
-           assertEquals(fin2, checkString2);
+      driver.findElement(By.xpath("//a[contains(@title,'Смартфон HONOR')]/ancestor::article//div[contains(@aria-label,'сравнению')]")).click();
+      logger.info("Добавился первый в списке HONOR");
+        String fin1 = "Товар " + driver.findElement(By.xpath("//a[contains(@title,'Смартфон HONOR')]")).getAttribute("title").toString() + " добавлен к сравнению";
+        String checkString1 = new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Товар Смартфон HONOR')]"))).getText();
+        assertEquals(fin1, checkString1);
+        logger.info("Проверили, что отобразилась плашка \"Товар HONOR добавлен к сравнению\"");
+
+
+      driver.findElement(By.xpath("//a[contains(@title,'Смартфон Xiaomi Redmi')]/ancestor::article//div[contains(@aria-label,'сравнению')]")).click();
+      logger.info("Добавился первый в списке Xiaomi");
+    //  String str2 = driver.findElement(By.xpath("//a[contains(@title,'Смартфон Xiaomi Redmi')]")).getAttribute("title").toString();
+      String fin2 = "Товар " + driver.findElement(By.xpath("//a[contains(@title,'Смартфон Xiaomi Redmi')]")).getAttribute("title").toString() + " добавлен к сравнению";
+      String checkString2 = new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Товар Смартфон Xiaomi Redmi')]"))).getText();
+      assertEquals(fin2, checkString2);
+      logger.info("Проверили, что отобразилась плашка \"Товар HONOR добавлен к сравнению\"");
 
           driver.findElement(By.xpath("//span[text()='Сравнить']")).click();
+          logger.info("Перешли в раздел Сравнение");
+
           List<WebElement> smartPhones = driver.findElements((By.xpath("//img[contains(@alt,'Смартфон ')]")));
-        assertEquals(2, smartPhones.size());
+          assertEquals(2, smartPhones.size());
+          logger.info("Проверили, что в списке товаров 2 позиции");
 
         driver.findElement(By.xpath("//button[text()='Все характеристики']")).click();
+        logger.info("Нажали на опцию \"все характеристики\"");
         driver.findElement(By.xpath("//div[text()='Операционная система']"));
+        logger.info("Проверили, что в списке характеристик появилась позиция \"Операционная система\"");
 
         driver.findElement(By.xpath("//button[text()='Различающиеся характеристики']")).click();
+        logger.info("Нажали на опцию \"различающиеся характеристики\"");
         assertEquals(0, driver.findElements(By.xpath("//div[text()='Операционная система']")).size());
+        logger.info("Проверили, что позиция \"Операционная система\" не отображается в списке характеристик");
 
-//<span class="_14Uuc5WvKg">Сравнить</span>
-     //   Actions action = new Actions(driver);
-    //    WebElement we = driver.findElement(By.xpath(locator));
-    //    action.moveToElement(we).moveToElement(driver.findElement(By.xpath("//img[contains(@alt,'Смартфон HONOR')]/parent::a/preceding-sibling::div/div[1]/div"))).click().build().perform();
-
-
-
-        // new WebDriverWait(driver,4).until(ExpectedConditions.visibilityOfElementLocated(new By.ByXPath("//article[8]/div[2]/div/div"))).click();
-
-      //  driver.findElement(By.xpath("//img[contains(@alt,'Смартфон HONOR')]/parent::a/preceding-sibling::div/div[1]/div")).click();
-   //     /html/body/div[2]/div[6]/div[2]/div/div[1]/div/div/div/article[6]/div[2]/div[1]/div
-
-    ///preceding-sibling::div/div[contains(@aria-label,'избранное')]/div
-        //    driver.findElement(By.xpath("//article[6]/div[2]/div/div")).click();
-     //   Товар Смартфон HONOR 7S 1/16GB добавлен к сравнению
-    //    asser
-     //   String actualString = driver.findElement(By.xpath("//div[contains(text(),'Товар Смартфон HONOR')]")).getText();
-     //   assertTrue(actualString.contains("добавлен к сравнению"));
-      //  driver.findElement(By.xpath("//article[9]/div[2]/div/div")).click();
-
-        //      href="/product--smartfon-honor
-  //    img  alt="Смартфон HONOR 7S 1/16GB"
-
-        //String expectedTitle = "Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям";
-        //assertEquals(expectedTitle,actualTitle);
-       // assertNotNull(actualTitle);
     }
-
-  /*  @Test
-    public void checkTitle() {
-        String actualTitle = driver.getTitle();
-        assertNotNull(actualTitle);
-        logger.info("Title не NULL на странице "+cfg.url());
-    } */
 
 
     @After
     public void setDown() {
         if (driver != null) {
-        //   driver.quit();
+           driver.quit();
         }
     }
 
 
 }
-//////////////////////////////////////////////////////////////////// ниже добавлено 26.07.2020//////////////////////
-/*
-public class SampleTest {
-    protected static WebDriver driver;
-    private Logger logger = LogManager.getLogger(SampleTest.class);
-    private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
-
-    ////////////Factory/////2nd homework///////////////////
-    interface Driver{
-        void driverCreation(String option);
-    }
-
-    class Chrome implements Driver{
-        @Override
-        public void driverCreation(String option){
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments(option);
-            driver = new ChromeDriver(options);
-            logger.info("Драйвер Chrome поднят");
-        }
-    }
-
-    class Firefox implements Driver{
-        @Override
-        public void driverCreation(String option){
-            WebDriverManager.firefoxdriver().setup();
-            FirefoxOptions options = new FirefoxOptions();
-            options.addArguments(option);
-            driver = new FirefoxDriver(options);
-            logger.info("Драйвер Firefox поднят");
-        }
-    }
-
-    class WebDriverFactory{
-        public Driver create(String typeOfDriver, String...options){
-            switch (typeOfDriver){
-                case "Chrome": return new Chrome(options[1], options[1]);
-                case "Firefox": return new Firefox();
-                default: return null;
-            }
-        }
-    }
-
-////////////Factory/////2nd homework///////////////////
-
-    /*@Test
-    public void Log(){
-
-    logger.info("this is info");
-    logger.error("this is error");
-    logger.fatal("this is fatal");
-    logger.debug("this is debug");
-    } */
-/*
-    @Before
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        driver = new ChromeDriver(options);
-        logger.info("Драйвер поднят");
-    }
-
-    @Test
-    public void cookieCreation(){
-        driver.get("https://otus.ru/");
-        driver.manage().addCookie(new Cookie("Otus1", "Value1"));
-        logger.info("Создан КУКИ Otus1 со значением "+ driver.manage().getCookieNamed("Otus1"));
-        driver.manage().addCookie(new Cookie("Otus2", "Value2"));
-        //logger.info("Создан КУКИ Otus2 со значением "+ driver.manage().getCookieNamed("Otus2"));
-        Cookie cookie=new Cookie("Otus3", "Value3");
-        driver.manage().addCookie(cookie);
-        //logger.info("Создан КУКИ Otus3 со значением "+ driver.manage().getCookieNamed("Otus3"));
-        driver.manage().addCookie(new Cookie("Otus4", "Value4"));
-        //logger.info("Создан КУКИ Otus4 со значением "+ driver.manage().getCookieNamed("Otus4"));
-        driver.manage().deleteCookieNamed("Otus2");
-        logger.info("удалили куки Otus2");
-        driver.manage().deleteCookie(cookie);
-        logger.info("удалили куки Otus2");
-        driver.manage().deleteAllCookies();
-        logger.info("Все КУКИ УДАЛЕНЫ. МАссив куков "+ driver.manage().getCookies().size());
-    }
-
-    // val tets:Int=0
-
-    @Test
-    public void setTimeOut() {
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.get(cfg.url());
-        logger.info("Открыта страница отус "+cfg.url());
-        driver.findElement(new By.ById("jivo_close_button")).click();
-        //logger.info("30 секунд открылся чат jcont el.isDisplayed()="+el.isDisplayed());
-
-    }
-
-
-
-    @Test
-    public void driverSize() {
-        driver.get(cfg.url());
-        driver.findElement(new By.ByLinkText("Рекрутерам")).click();
-        //driver.manage().window().maximize();
-        //logger.info("Открыта страница отус "+driver.manage().window().getSize());
-        //Dimension demension = new Dimension(800, 600);
-        //driver.manage().window().setSize(demension);
-        //logger.info("Открыта страница отус "+driver.manage().window().getSize());
-    }
-
-    @Test
-    public void yanTest() {
-        driver.get("https://ya.ru");
-        driver.findElement(new By.ById("text")).sendKeys("otus");
-        driver.findElement(new By.ById("text")).sendKeys(Keys.ENTER);
-        // driver.findElement(new By.ById("text")).;
-        //driver.findElement(new By.ByClassName("button suggest2-form__button button_theme_websearch button_size_xl i-bem button_js_inited")).click();
-        new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(new By.ByClassName("organic__url-text"))).click();
-        //   new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(By.xpath("//"))).click();
-        //driver.manage().window().maximize();
-        //logger.info("Открыта страница отус "+driver.manage().window().getSize());
-        //Dimension demension = new Dimension(800, 600);
-        //driver.manage().window().setSize(demension);
-        //logger.info("Открыта страница отус "+driver.manage().window().getSize());
-    }
-
-    @After
-    public void setDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-
-}
-*/
