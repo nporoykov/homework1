cc6a2513079f7d023b1a1f4f15e3656666fac907
nporoykov <nporoykov@gmail.com>
2020-10-18 22:05:18 +0300

    hw 1st commit

--- src/test/java/utils/BaseHooks.java
+++ src/test/java/utils/BaseHooks.java
@@ -8,8 +8,13 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import pages.HomeworkTest;
 
+import javax.print.DocFlavor;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class BaseHooks {
@@ -17,8 +22,17 @@ public class BaseHooks {
     protected static ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
 
     @BeforeClass
-    public static void setup() {
-        driver = WebDriverFactory.createDriver(WebDriverType.FIREFOX);
+    public static void setup() throws MalformedURLException {
+        String selenoidURL = "http://localhost:4444/wd/hub";
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("firefox");
+        caps.setVersion("80.0");
+        caps.setCapability("enableVNC", true);
+        caps.setCapability("screenResolution", "1280x1024");
+
+        driver = new RemoteWebDriver(new URL(selenoidURL), caps);
+
+       // driver = WebDriverFactory.createDriver(WebDriverType.FIREFOX);
 
         if (driver != null) {
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
